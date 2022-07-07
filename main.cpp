23fb9709b09b1ade881ed2030cec36e6c92eaf60
jkaar <96846917+jkaar@users.noreply.github.com>
2022-07-07 14:00:37 +0100

    Update main.cpp
    Added function for gradient change for an increase in particles.
    An attempt at dynamically creating objects to simulate a large number of particles (incomplete).
    The program currently simulates two particles and two gravity sources.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
 #include <SFML/Graphics.hpp>
 #include"GravitySource.h"
 #include"Particle.h"
+#include <iostream>
+#include <random>
+#include <vector>
+
+
+int whitetored(int n) {
+    int r, g, b;
+    r = 255;
+    g = 255 - n;
+    b = g;
+    return r, g, b;
+};
+
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1600, 1000), "My Program");
+    
+
+    sf::RenderWindow window(sf::VideoMode(1600, 1000), "SFML-Gravity");
     window.setFramerateLimit(60);
 
-    GravitySource source(800, 500, 7000);
-
-    Particle particle(600, 700, 4, 0);
-
+    GravitySource source(400, 500, 6000);
+    GravitySource source2(1200, 500, 6000);
+   
+    Particle particle(600, 700, 4, 1);
+    Particle particle2(600, 500, 4, 2);
+    
     while (window.isOpen())
     {
         sf::Event event;
@@ -23,11 +40,33 @@ int main()
 
 
         window.clear();
-        particle.update_physics(source);
 
+       /*
+        int n = 0; // number of particles
+        
+        Particle* particle[4];
+        for (int i = 0; i < sizeof(particle) / sizeof(*particle); i++) {
+            particle[i] = new Particle(600 + i, 700 - 2*i, 4, 0);
+            std::cout << particle[i] << std::endl;
+            //Particle obj = particle[i];
+            //obj.update_physics(source);
+        } 
+
+        for (int i = 0; i < sizeof(particle) / sizeof(*particle); i++)
+            delete particle[i];  
+        
+        */
+      
+        particle.update_physics(source);
+        particle.update_physics(source2);
+        particle2.update_physics(source);
+        particle2.update_physics(source2);
         source.render(window);
+        source2.render(window);
         particle.render(window);
-        //draw calls
+        particle2.render(window);
+        
+        // draw calls
         window.display();
     }
 
